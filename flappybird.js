ff51f0a922d57428b91382f755ff73a22e11e08b
omar_khaled_72 <omarkhaled202080@gmail.com>
2025-04-07 23:57:39 +0200

    first publish

---
+++ flappybird.js
@@ -0,0 +1,186 @@
+//board
+let board;
+let boardWidth = 360;
+let boardHeight = 640;
+let context;
+
+//bird
+let birdWidth = 34;
+let birdHeight = 24;
+let birdX = boardWidth / 8;
+let birdY = boardHeight / 2;
+let birdImg;
+
+let bird = {
+  x: birdX,
+  y: birdY,
+  width: birdWidth,
+  height: birdHeight,
+};
+
+//pipes
+let pipeArray = [];
+let pipeWidth = 64;
+let pipeHeight = 512;
+let pipeX = boardWidth;
+let pipeY = 0;
+
+let topPipeImg;
+let bottomPipeImg;
+
+//physics
+let velocityX = -2; //pipes moving left speed
+let velocityY = 0; //bird jump speed
+let gravity = 0.2; // Reduced gravity
+let jumpForce = -6; // Jump strength
+
+let gameOver = false;
+let score = 0;
+
+window.onload = function () {
+  board = document.getElementById("board");
+  board.height = boardHeight;
+  board.width = boardWidth;
+  context = board.getContext("2d");
+
+  //load images
+  birdImg = new Image();
+  birdImg.src = "./flappybird.png";
+
+  topPipeImg = new Image();
+  topPipeImg.src = "./toppipe.png";
+
+  bottomPipeImg = new Image();
+  bottomPipeImg.src = "./bottompipe.png";
+
+  // Initial jump when game starts
+  velocityY = jumpForce;
+
+  requestAnimationFrame(update);
+  setInterval(placePipes, 1500);
+  document.addEventListener("keydown", moveBird);
+};
+
+function update() {
+  requestAnimationFrame(update);
+  if (gameOver) {
+    return;
+  }
+  context.clearRect(0, 0, board.width, board.height);
+
+  //bird physics
+  velocityY += gravity;
+  bird.y += velocityY;
+
+  // Draw bird if image is loaded
+  if (birdImg.complete) {
+    context.drawImage(birdImg, bird.x, bird.y, bird.width, bird.height);
+  } else {
+    // Fallback rectangle if image not loaded
+    context.fillStyle = "red";
+    context.fillRect(bird.x, bird.y, bird.width, bird.height);
+  }
+
+  // Boundary check
+  if (bird.y <= 0) {
+    bird.y = 0;
+    velocityY = 0;
+  }
+
+  if (bird.y + bird.height >= board.height) {
+    gameOver = true;
+  }
+
+  //pipes
+  for (let i = 0; i < pipeArray.length; i++) {
+    let pipe = pipeArray[i];
+    pipe.x += velocityX;
+
+    if (pipe.img.complete) {
+      context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height);
+    }
+
+    if (!pipe.passed && bird.x > pipe.x + pipe.width) {
+      score += 0.5;
+      pipe.passed = true;
+    }
+
+    if (detectCollision(bird, pipe)) {
+      gameOver = true;
+    }
+  }
+
+  //clear pipes
+  while (pipeArray.length > 0 && pipeArray[0].x < -pipeWidth) {
+    pipeArray.shift();
+  }
+
+  //score
+  context.fillStyle = "white";
+  context.font = "45px sans-serif";
+  context.fillText(score, 5, 45);
+
+  if (gameOver) {
+    context.fillStyle = "red";
+    context.font = "30px sans-serif";
+    context.fillText("GAME OVER", boardWidth / 2 - 100, boardHeight / 2);
+  }
+}
+
+function placePipes() {
+  if (gameOver) {
+    return;
+  }
+
+  let randomPipeY = pipeY - pipeHeight / 4 - Math.random() * (pipeHeight / 2);
+  let openingSpace = board.height / 3; // Increased gap between pipes
+
+  let topPipe = {
+    img: topPipeImg,
+    x: pipeX,
+    y: randomPipeY,
+    width: pipeWidth,
+    height: pipeHeight,
+    passed: false,
+  };
+  pipeArray.push(topPipe);
+
+  let bottomPipe = {
+    img: bottomPipeImg,
+    x: pipeX,
+    y: randomPipeY + pipeHeight + openingSpace,
+    width: pipeWidth,
+    height: pipeHeight,
+    passed: false,
+  };
+  pipeArray.push(bottomPipe);
+}
+
+function moveBird(e) {
+  if (e.code == "Space" || e.code == "ArrowUp" || e.code == "KeyX") {
+    //jump
+    velocityY = jumpForce;
+
+    //reset game
+    if (gameOver) {
+      resetGame();
+    }
+  }
+}
+
+function resetGame() {
+  bird.y = birdY;
+  pipeArray = [];
+  score = 0;
+  gameOver = false;
+  velocityY = jumpForce; // Initial jump when game resets
+}
+
+function detectCollision(a, b) {
+  return (
+    a.x < b.x + b.width &&
+    a.x + a.width > b.x &&
+    a.y < b.y + b.height &&
+    a.y + a.height > b.y
+  );
+}
